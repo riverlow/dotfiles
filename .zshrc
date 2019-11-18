d6a4de77f15e219baa0ca6359a0e18422d0daad8
x <x@y>
2019-11-18 23:58:58 +0800

    a

--- .zshrc
+++ .zshrc
@@ -1,5 +1,5 @@
 ### Added by Zplugin's installer
-source '/Users/x/.zplugin/bin/zplugin.zsh'
+source "$HOME/.zplugin/bin/zplugin.zsh"
 autoload -Uz _zplugin
 (( ${+_comps} )) && _comps[zplugin]=_zplugin
 ### End of Zplugin's installer chunk
--- myscripts/rsync-offline.sh
+++ myscripts/rsync-offline.sh
@@ -1,8 +1,26 @@
+#!/usr/bin/zsh
+
 DEST=${HOME}/offline/
+mkdir -p $DEST
+
+# zplugin
+mkdir -p ~/.zplugin
+git clone https://github.com/zdharma/zplugin.git ~/.zplugin/bin
+zcompile ~/.zplugin/bin/zplugin.zsh
+
+# tpm
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# vim-plug
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # cause option '-R', must let pwd to be $HOME
 cd $HOME
+rsync -avR .zplugin $DEST
+rsync -avR .tmux/plugins $DEST
 rsync -avR .local/share/nvim/site/autoload/plug.vim $DEST
+rsync -avR .local/lib $DEST
 rsync -avR .vim/plugged $DEST
 rsync -avR .fzf $DEST
-rsync -avR .tmux/plugins $DEST
+rsync -avR go $DEST
