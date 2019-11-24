703350530d13dcb44b1aaa043f490d617800ca37
x <x@y>
2019-11-24 23:38:18 +0800

    a

--- myscripts/rsync-offline.sh
+++ myscripts/rsync-offline.sh
@@ -3,6 +3,7 @@
 DEST=${HOME}/offline/
 mkdir -p $DEST
 
+if [[ $1 == "init" ]];then
 # zplugin
 mkdir -p ~/.zplugin
 git clone https://github.com/zdharma/zplugin.git ~/.zplugin/bin
@@ -14,6 +15,7 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 # vim-plug
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
 
 # cause option '-R', must let pwd to be $HOME
 cd $HOME
@@ -23,4 +25,7 @@ rsync -avR .local/share/nvim/site/autoload/plug.vim $DEST
 rsync -avR .local/lib $DEST
 rsync -avR .vim/plugged $DEST
 rsync -avR .fzf $DEST
+rsync -avR .dotfiles $DEST
 rsync -avR go $DEST
+rsync -avR sdk $DEST
+rsync -avR myfonts $DEST
