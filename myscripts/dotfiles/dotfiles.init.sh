be8c45b8448d31a78907057e3a1c3215cd3fa0a8
x <x@y>
2021-02-05 03:47:36 -0600

    a

--- myscripts/dotfiles/dotfiles.init.sh
+++ myscripts/dotfiles/dotfiles.init.sh
@@ -3,9 +3,16 @@
 # First step
 git clone --bare https://github.com/iofxl/dotfiles.git $HOME/.dotfiles
 
-if [ $? = 0 ]; then
+if [ $? != 0 ]; then
 	echo "git error.";
 	exit 1;
+fi
+
+which rsync
+
+if ! command -v rsync; then
+	echo "no command rsync.";
+	exit 2;
 fi
 
 function dotfiles {
@@ -19,7 +26,7 @@ if [ $? = 0 ]; then
   echo "Checked out config.";
 else
   echo "Backing up pre-existing dot files.";
-  dotfiles checkout 2>&1 | awk '/^\s+/ { print $1}' | xargs -I{} rsync -avR --remove-source-files {} .dotfiles-backup
+  dotfiles checkout 2>&1 | awk '/^\s+/ { print $1}' | xargs -I{} rsync -avR --remove-source-files {} .dotfiles-backup/
 fi;
 dotfiles checkout
 dotfiles config status.showUntrackedFiles no
