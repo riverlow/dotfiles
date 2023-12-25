b84966d59c578ebf8cb2bac0989bc62cfa74afa6
x <x@y>
2023-12-25 11:30:59 -0600

    a

--- .config/fish/conf.d/macOS.fish
+++ .config/fish/conf.d/macOS.fish
@@ -4,7 +4,10 @@ if test (uname) = "Darwin";
     fish_add_path $HOME/myscripts/bin_macos
 
     # PATH
-    fish_add_path /usr/local/opt/curl/bin /usr/local/opt/gawk/libexec/gnubin /usr/local/opt/iftop/sbin /usr/local/opt/unzip/bin /usr/local/opt/coreutils/libexec/gnubin /usr/local/opt/findutils/libexec/gnubin /usr/local/opt/gnu-tar/libexec/gnubin /usr/local/opt/gnu-indent/libexec/gnubin /usr/local/opt/grep/libexec/gnubin 
+    fish_add_path /usr/local/opt/curl/bin /usr/local/opt/gawk/libexec/gnubin /usr/local/opt/iftop/sbin /usr/local/opt/unzip/bin /usr/local/opt/coreutils/libexec/gnubin /usr/local/opt/findutils/libexec/gnubin /usr/local/opt/gnu-tar/libexec/gnubin /usr/local/opt/gnu-indent/libexec/gnubin /usr/local/opt/grep/libexec/gnubin /usr/local/opt/make/libexec/gnubin/ /usr/local/opt/libpcap/bin
+
+    # homebrew
+    set -gx HOMEBREW_FORCE_BREWED_CURL 1
 
     # Java
     fish_add_path '/usr/local/opt/openjdk/bin' 
@@ -18,11 +21,18 @@ if test (uname) = "Darwin";
     # llvm
     fish_add_path /usr/local/opt/llvm/bin
 
+    # bison
+    fish_add_path /usr/local/Cellar/bison/3.8.2/bin/
+
     # qt5
     fish_add_path /usr/local/opt/qt@5/bin
 
     # pyenv
     pyenv init - --no-rehash| source
+
+    # wine
+    set -gx WINEPREFIX ~/WINE_Library 
+    set -gx DYLD_FALLBACK_LIBRARY_PATH $DYLD_FALLBACK_LIBRARY_PATH:/usr/lib:/usr/X11/lib
 
 end
 
--- myscripts/bin/rsync.sh
+++ myscripts/bin/rsync.sh
@@ -1,12 +1,13 @@
 #!/usr/local/bin/bash
 #
 
+mediadir=~/lmedia/
 if [[ $1 == "movie" ]]; then
-    rsync -av --progress  -e ssh "${2}" lu:Movies/localmovies
+    rsync -av --progress  -e ssh "${2}" lu:$mediadir/movies
 fi
 
 
 if [[ $1 == "show" ]]; then
-    rsync -av --progress  -e ssh "${2}" lu:Movies/shows
+    rsync -av --progress  -e ssh "${2}" lu:$mediadir/shows
 fi
 
