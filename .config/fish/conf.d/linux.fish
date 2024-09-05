cc45bf547cb12337c035fe57503b9873ddd14adb
x <x@y>
2024-09-05 01:33:48 -0500

    a

--- .config/fish/conf.d/linux.fish
+++ .config/fish/conf.d/linux.fish
@@ -2,6 +2,7 @@ if test (uname) = "Linux";
 
     # alias
     alias sl     systemctl
+    alias rl     resolvectl
 
     # fzf
     function fish_user_key_bindings
--- myscripts/bin_macos/setdns.sh
+++ myscripts/bin_macos/setdns.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 
+if [ "$2" = "dns" ]; then
 sudo networksetup -setdnsservers Wi-Fi empty
-sudo networksetup -setdnsservers Wi-Fi 127.0.0.1
+sudo networksetup -setdnsservers Wi-Fi 192.168.89.64
+fi
+
+if [ "$2" = "show" ]; then
+    scutil --dns | grep 'nameserver\[[0-9]*\]'
+fi
+
+if [ "$2" = "search" ]; then
+    networksetup -setsearchdomains Wi-Fi lan
+fi
+
 # flush dns cache
 sudo killall -HUP mDNSResponder
 sudo killall mDNSResponderHelper
 sudo dscacheutil -flushcache
-
-scutil --dns | grep 'nameserver\[[0-9]*\]'
